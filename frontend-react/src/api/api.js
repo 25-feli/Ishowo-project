3fd9dc7f6da964639417d76b69d80ac4521819aa
25-feli <feliciteagonman@gmail.com>
2026-07-06 17:15:16 +0100

    Modifs

--- frontend-react/src/api/api.js
+++ frontend-react/src/api/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // Pour le développement local
-const API_URL = 'http://localhost:8000/api';
+//const API_URL = 'http://localhost:8000/api';
 
 // Pour la production (Iwaju Tech)
-//const API_URL = 'https://ishowo-api.iwajutech.com/api';
+const API_URL = 'https://ishowo-api.iwajutech.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
